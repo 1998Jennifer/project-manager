0c966cc9449aa809fcee8bddae1b2449b5dbe23d
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-03-14 17:50:51 -0500

    Inicio de proyecto

---
+++ database/migrations/2022_03_14_022916_create_projects_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('city_id')->unsigned();
+            $table->bigInteger('company_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('name');
+            $table->dateTime('execution_date');
+            $table->integer('is_active');
+            $table->timestamps();
+
+            // // 
+            // $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('city_id')->references('id')->on('cities');
+            // $table->foreign('company_id')->references('id')->on('companies');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
